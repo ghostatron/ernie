9da246b2255676e093c38e1c73d68e57914461f9
Randy Haid <Randy.Haid@walmartlabs.com>
2017-10-27 14:48:39 -0700

    Initial commit (basically just the vanilla starter template via Xcode).

---
+++ Ernie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ernie
+//
+//  Created by Randy Haid on 10/27/17.
+//  Copyright © 2017 Randy Haid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
