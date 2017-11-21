21695f7c0f5f0b617679f4854bf49ec44e40b016
Randy Haid <Randy.Haid@walmartlabs.com>
2017-11-20 16:08:28 -0800

    Slapped down the storyboard UI for Containers.

---
+++ Ernie/UI/Containers/ContainersTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContainerMiniAppTableViewCell.swift
+//  Ernie
+//
+//  Created by Randy Haid on 11/20/17.
+//  Copyright © 2017 Randy Haid. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ContainersTableViewCell: NSTableCellView
+{
+    
+}
