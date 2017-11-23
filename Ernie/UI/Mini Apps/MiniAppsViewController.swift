ed6157fe20a9a29bd647f86feceababcedaa7937
Randy Haid <Randy.Haid@walmartlabs.com>
2017-11-22 16:31:35 -0800

    Connected the outlets for the Mini Apps view controller.

--- Ernie/UI/Mini Apps/MiniAppsViewController.swift
+++ Ernie/UI/Mini Apps/MiniAppsViewController.swift
@@ -11,5 +11,26 @@ import Cocoa
 
 class MiniAppsViewController: NSViewController
 {
+    /// The data source for the mini apps table.
+    var allMiniAppsAlphabetized: [MiniApp] = []
     
+    /// The data source for the containers table.
+    var selectedMiniAppContainersAlphabetized: [Container] = []
+    
+    // MARK:- IBOutlet Properties
+
+    @IBOutlet weak var editButton: NSButton!
+    @IBOutlet weak var miniAppNameLabel: NSTextField!
+    @IBOutlet weak var miniAppFolderLabel: NSTextField!
+    @IBOutlet weak var miniAppDescriptionLabel: NSTextField!
+    @IBOutlet weak var miniAppsTableView: NSTableView!
+    @IBOutlet weak var containersTableView: NSTableView!
+    
+    @IBAction func addMiniAppButtonPressed(_ sender: NSButton)
+    {
+    }
+    
+    @IBAction func editButtonPressed(_ sender: NSButton)
+    {
+    }
 }
