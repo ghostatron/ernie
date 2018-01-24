200dcf2a87f9d0305a6781c4a12a730e78a4b946
Randy Haid <Randy.Haid@walmartlabs.com>
2018-01-24 10:53:59 -0800

    More swagger container UI elements in place.

---
+++ Ernie/UI/Swagger/Container/Editor/SwaggerContainerEditorViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SwaggerContainerEditorViewController.swift
+//  Ernie
+//
+//  Created by Randy Haid on 1/24/18.
+//  Copyright © 2018 Randy Haid. All rights reserved.
+//
+
+import Cocoa
+
+class SwaggerContainerEditorViewController: NSViewController
+{
+    
+}
