37dcedb5a62a6cdd84f19aaa1d6ed809472d803a
Randy Haid <Randy.Haid@walmartlabs.com>
2018-02-21 15:19:39 -0800

    Laid out UI elements for API generation feature.

---
+++ Ernie/UI/Swagger/Container/Editor/APIGenerationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  APIGenerationViewController.swift
+//  Ernie
+//
+//  Created by Randy Haid on 2/21/18.
+//  Copyright © 2018 Randy Haid. All rights reserved.
+//
+
+import Cocoa
+
+class APIGenerationViewController: NSViewController
+{
+    
+}
---
+++ Ernie/UI/Swagger/Container/Editor/ImplementationGenerationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImplementationGenerationViewController.swift
+//  Ernie
+//
+//  Created by Randy Haid on 2/21/18.
+//  Copyright © 2018 Randy Haid. All rights reserved.
+//
+
+import Cocoa
+
+class ImplementationGenerationViewController: NSViewController
+{
+    
+}
